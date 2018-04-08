58749e38f8f220ea8a412abd8c5c2eb9dd186e61
Unknown <mnn264@nyu.edu>
2018-04-08 03:30:34 -0400

    so...CLOSE
    almost

---
+++ Assets/CloudController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudController : MonoBehaviour {
+
+	public Transform player;
+	public float speed;
+	public float startPos;
+	public float endPos;
+	public float maxHeight;
+	public float minHeight;
+
+	// Use this for initialization
+	void Start () {
+
+		player = Master.me.player.transform;
+		speed = Random.Range(.5f, 2f) / 100;
+		transform.position = new Vector2(Random.Range(player.position.x, startPos), player.position.y + Random.Range(minHeight, maxHeight));
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position = new Vector2(transform.position.x - speed, transform.position.y);
+
+		if (transform.position.x < endPos) {
+			transform.position = new Vector2(startPos, player.position.y + Random.Range(minHeight, maxHeight));
+		}
+
+		
+	}
+}
